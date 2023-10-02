93e2177e94a978ea2a51568fcbf5269835c52b22
BABEL-SI\alvaro.rueda <alvaro.rueda@babel.es>
2023-10-02 11:59:07 +0200

    Primeng installation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'pwa-angular';
+
+  items: MenuItem[];
+
+  constructor() {
+    this.items = [
+      {
+        label: 'Inicio',
+        icon: 'pi pi-home',
+        routerLink: ['/home'],
+      },
+      {
+        label: 'Productos',
+        icon: 'pi pi-list',
+        routerLink: ['/products'],
+      },
+      {
+        label: 'Servicios',
+        icon: 'pi pi-cog',
+        routerLink: ['/services'],
+      },
+      {
+        label: 'Contacto',
+        icon: 'pi pi-envelope',
+        routerLink: ['/contact'],
+      },
+    ];
+  }
 }
